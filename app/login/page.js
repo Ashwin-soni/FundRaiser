55ace6f4ff9cf8bda944c950fafb4285b62bb914
Ashwin-soni <ashwin21122soni@gmail.com>
2025-09-15 01:06:14 +0530

    first

---
+++ app/login/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='text-white py-14 container mx-auto'>
+        <h1 className='text-center font-bold text-3xl'>Login/Signup to Get your fans to support you</h1>      
+
+        <div className='social-login-buttons'>
+
+        </div>
+    </div>
+  )
+}
+
+export default Login
